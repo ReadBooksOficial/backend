99000d85ab8643439edbef614b017e793c12b163
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-03-05 13:00:49 -0300

    fix: usar modelo UserToken para permissoes do banco

--- app/Http/Middleware/CheckUserToken.php
+++ app/Http/Middleware/CheckUserToken.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\UserToken;
 
 class CheckUserToken
 {
@@ -20,7 +21,8 @@ class CheckUserToken
         $token = $request->bearerToken();
     
         // Verifica se o token existe na tabela
-        $tokenExists = \DB::table('pacoca.user_tokens')->where('user_id', $user->id)->where('token', $token)->exists();
+        // $tokenExists = \DB::table('pacoca.user_tokens')->where('user_id', $user->id)->where('token', $token)->exists();
+        $tokenExists = UserToken::where('user_id', $user->id)->where('token', $token)->exists();
     
         if (!$tokenExists) {
             return response()->json(['message' => 'Unauthorized'], 401);
