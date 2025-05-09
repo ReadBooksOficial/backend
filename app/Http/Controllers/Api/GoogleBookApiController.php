dab6e3517ab76e54624eb65035570100eeebc816
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-05-09 10:12:02 -0300

    refact1

--- app/Http/Controllers/Api/GoogleBookApiController.php
+++ app/Http/Controllers/Api/GoogleBookApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use App\Http\Controllers\LivroController;
 use App\Models\Livro;
