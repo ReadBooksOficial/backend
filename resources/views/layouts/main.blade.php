12401af6aef338318166fed95b0d4ca9306af161
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-05-16 20:04:54 -0300

    style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
     <title>@yield('title')</title>
      <script src="https://unpkg.com/feather-icons"></script>
      <script src="https://unpkg.com/lucide@latest"></script>
-    <link rel="stylesheet" href="{{asset('css/style.css?v=12')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css?v=13')}}">
     <link rel="stylesheet" href="{{asset('css/vars.css')}}">
     <link rel="shortcut icon" href="{{asset('img/estante_icon.png')}}" type="image/x-icon">
     <link href="{{asset('bootstrap-5.0.0/cdn.jsdelivr.net_npm_bootstrap@5.0.2_dist_css_bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
