c1868cc88d2ff52b81c129c0aacdee44789a047e
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-06-12 17:07:40 -0300

    style: conta

--- resources/views/conta/conta.blade.php
+++ resources/views/conta/conta.blade.php
@@ -6,53 +6,27 @@
 <div class="container container-conta">
     <div class="row justify-content-center">
         <div class="col-md-8">
-                <h1 class="text-center" style="margin-bottom: 20px; font-size: 40px">Conta</h1>
-                <p>Atualize as informações da sua conta pelo central de contas: <a href="{{config("app.pacoca_url")}}/conta" target="_blank" rel="noopener noreferrer">{{config("app.pacoca_url")}}/conta</a></p>
+            <h1 class="text-center" style="margin-bottom: 20px; font-size: 40px">Conta</h1>
+            <p>Atualize as informações da sua conta pelo <a href="{{config("app.pacoca_url")}}/conta" target="_blank" rel="noopener noreferrer">Central de Contas</a> Alterações são refletidas no Paçoca, RITA e ReadBooks!    </p>
 
-                {{-- NOME --}}
-                <div class="form-group row mb-3">
+            {{-- NOME --}}
+            <div class="form-group row mb-3">
+                <div class="col">
                     <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
-                    <div class="col">
-                        <input disabled placeholder="Email ou nome de usuário" id="email" type="login" class="input-login form-control @error('login') is-invalid @enderror" name="login" value="{{ auth()->user()->name }}" autocomplete="login" autofocus>
-                    </div>
-
-                    @error('name')
-                        <span class="invalid-feedback" role="alert" style="display: block!important">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                    <input disabled placeholder="Email ou nome de usuário" id="email" type="login" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ auth()->user()->name }}" autocomplete="login" autofocus>
                 </div>
+            </div>
 
-
-
-                {{-- EMAIl --}}
-                <div class="form-group row mb-3">
+            {{-- EMAIl --}}
+            <div class="form-group row mb-3">
+                <div class="col">
                     <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
-                    <div class="col">
-                        <input disabled placeholder="Email ou nome de usuário" id="email" type="login" class="input-login form-control @error('login') is-invalid @enderror" name="login" value="{{ auth()->user()->email }}" autocomplete="login" autofocus>
-                    </div>
-
-                    @error('email')
-                        <span class="invalid-feedback" role="alert" style="display: block!important">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                    <input disabled placeholder="Email ou nome de usuário" id="email" type="login" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ auth()->user()->email }}" autocomplete="login" autofocus>
                 </div>
-
-
-                <div class="form-group row mt-4 mb-0">
-                       <div class="col">
-                        <a href="/logout" class="btn btn-atualizar-conta btn-outline-danger" style="width: 100%">
-                            Sair
-                        </a>
-                       </div>
-                    </div>
-                </div>
+            </div>
         </div>
-
     </div>
-
 </div>
 
 
